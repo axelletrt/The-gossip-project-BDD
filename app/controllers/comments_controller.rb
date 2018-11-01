4a8ca8978cb8a8b5011dcca1466d10d781df8159
Axelle Tortai <a.tortai14@gmail.com>
2018-11-01 22:58:59 +0100

    J'en peux pluuuuus

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,21 +8,29 @@ class CommentsController < ApplicationController
         @comment = Comment.new 
     end 
 
-     def create
+    def create
         @gossip = Gossip.find(params[:gossip_id])
-        @comment = @gossip.comments.create(comment_params)
+    
+        @comment = @gossip.comments.new(comment_params)
+        @comment.user_id = session[:user_id]
+        @comment.save 
         redirect_to gossip_path(@gossip)
-      end
-     
-      private
-        def comment_params
-          params.require(:comment).permit(:user_id, :body)
-        end
+    end
 
-        def show
-            @comment = Comment.find(id: params[:comment_id])
-        end
+      private
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+
+    def show
+        @comment = Comment.find(id: params[:comment_id])
+    end
 end
+
+
+
+
+
 
 
 
